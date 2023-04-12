c823c471158fea9a2990e4e675a24df7f45c7edf
lui154 <luisa2020cc@gmail.com>
2023-04-11 21:58:13 -0300

    condicionais js

--- JS/primeiros_passos.js
+++ JS/primeiros_passos.js
@@ -1,9 +1,15 @@
-const preCombustivel = 4.53;
+const precoGas = 5.60;
+const precoEta = 4.50;
 const gastoMedio = 10;
 const distKM = 113;
 
 const consumo = distKM / gastoMedio;
-const gasto = consumo * preCombustivel;
+const tipoComb = 'Gasolina';
 
-console.log(gasto);
-console.log(gasto.toFixed(2));
+if(tipoComb === 'Etanol'){
+    const valorGasto = consumo * precoEta;
+    console.log(valorGasto.toFixed(2));
+}else{
+    const valorGasto = consumo * precoGas;
+    console.log(valorGasto.toFixed(2));
+}
