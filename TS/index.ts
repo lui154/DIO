44859903f8ed52d882b14f007aa17b0bc9ed51ca
lui154 <luisa2020cc@gmail.com>
2023-04-16 20:45:05 -0300

    novo

--- TS/index.ts
+++ TS/index.ts
@@ -2,12 +2,59 @@ type Hero = {
     name: string;
     vulgo: string;
 };
-
 function printaObj(pessoa: Hero){
     console.log(pessoa);
 }
-
 printaObj({
     name:"bruce",
     vulgo: "batman",
 });
+// Funçoes
+function addNum(x: number, y: number){
+    return x+y;
+}
+function addHelo(name: string){
+    return `Hello ${name}`;
+}
+let soma: number = addNum(4,7);
+console.log(soma);
+console.log(addHelo("Luisa"))
+// Funçoes mult tipos
+ function callPhone(phone:number|string):number|string{
+    return phone;
+ }
+ console.log(callPhone(9915544));
+ // Funçoes Async
+async function getData(id:number):Promise<string> {
+    return "luisa";
+}
+//Interface
+interface robot{
+    readonly id: number|string;
+    name: string;
+    sayHello():string;
+}
+const bot:robot={
+    id:"1",
+    name: "robbie",
+    sayHello():string{
+        return "Hello World";
+    }
+}
+console.log(bot.id)
+console.log(bot.name = "Robert");
+
+class Robo implements robot{
+    id: string | number;
+    name: string;
+    
+    constructor(id: string|number, name:string){
+        this.id = id;
+        this.name = name;
+    }
+    sayHello():string{
+        return `Hello i'm ${this.name}`;
+    }
+}
+const r = new Robo(1, "Elliot");
+console.log(r.sayHello());
--- biuld/index.js
+++ biuld/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 function printaObj(pessoa) {
     console.log(pessoa);
 }
@@ -6,3 +15,44 @@ printaObj({
     name: "bruce",
     vulgo: "batman",
 });
+// Funçoes
+function addNum(x, y) {
+    return x + y;
+}
+function addHelo(name) {
+    return `Hello ${name}`;
+}
+let soma = addNum(4, 7);
+console.log(soma);
+console.log(addHelo("Luisa"));
+// Funçoes mult tipos
+function callPhone(phone) {
+    return phone;
+}
+console.log(callPhone(9915544));
+// Funçoes Async
+function getData(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return "luisa";
+    });
+}
+const bot = {
+    id: "1",
+    name: "robbie",
+    sayHello() {
+        return "Hello World";
+    }
+};
+console.log(bot.id);
+console.log(bot.name = "Robert");
+class Robo {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello() {
+        return `Hello i'm ${this.name}`;
+    }
+}
+const r = new Robo(1, "Elliot");
+console.log(r.sayHello());
