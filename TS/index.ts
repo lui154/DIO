ab730f4abe222224ce026a722ecafb20994b4355
lui154 <luisa2020cc@gmail.com>
2023-04-16 22:17:09 -0300

    decorators

--- TS/index.ts
+++ TS/index.ts
@@ -1,3 +1,63 @@
+//Decorators
+function miniLength(length:number){
+    return(target:any, key:string)=>{
+        let _value = target[key];
+        const getter = () => _value;
+        const setter = (value:string)=>{
+            if (value.length<length){
+                throw new Error(`Tamanho menor que ${length}`);
+            }else{
+                _value = value;
+            }
+        };
+        Object.defineProperty(target, key,{
+            get:getter, set:setter,});
+    };
+}
+class Api{
+    @miniLength(3)
+    name:string;
+    constructor(name: string){
+        this.name = name;
+    }
+}
+const api = new Api("prod");
+console.log(api.name);
+/*//Genericos
+function concatArray<T>(...itens: T[]): T[]{
+    return new Array().concat(...itens);
+}
+const numArray = concatArray<number[]>([1,5,6],[3]);
+const stgArray = concatArray<string[]>(["bruce", "diana"],["clark"]);
+console.log(numArray);
+console.log(stgArray);
+//Classes
+class Character{
+    protected name?: string;
+    stregth: number;
+    skill: number;
+
+    constructor(name:string, stregth:number, skill:number){
+        this.name = name;
+        this.stregth = stregth;
+        this.skill = skill;
+    }
+    attack():void{
+        console.log(`Attack whit ${this.stregth} points`);
+    }
+}
+class Magician extends Character{
+    magicPoints: number;
+    constructor(name:string, stregth:number, skill:number,magicPoints: number){
+        super(name, stregth, skill);
+        this.magicPoints = magicPoints;        
+    }
+}
+
+const p1 = new Character("Ryu", 10, 98);
+const p2 = new Magician("Presto", 5, 30, 100);
+p1.attack();
+
 type Hero = {
     name: string;
     vulgo: string;
@@ -56,4 +116,4 @@ class Robo implements robot{
     }
 }
 const r = new Robo(1, "Elliot");
-console.log(r.sayHello());
+console.log(r.sayHello());*/
